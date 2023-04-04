9ab176fe35912737dfba0d7b357fc591c067bd13
tiagorosapc <tiagorosapc@gmail.com>
2023-04-04 09:14:46 -0300

    Versão final com requisitos obrigatórios e função de seleção de itens otimizada

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 
 let tPrato, tBebida, tSobremesa;
+let pPrato, pBebida, pSobremesa;
 
 function liberarBotaoFinalizaPedido(){
 
@@ -25,7 +26,36 @@ function liberarBotaoFinalizaPedido(){
     }
 }
 
-function selecionarPrato(prato){
+function selecionarItem(item, seletor){
+    
+    console.log(`${seletor} .selecionado`);
+
+    // 1 - verificar se existe um item selecionado anteriormente
+    const itemJaSelecionado = document.querySelector(`${seletor} .selecionado`);
+    // 2 - se existir, o item deve ser desmarcado 
+    if ( itemJaSelecionado !== null){
+        itemJaSelecionado.classList.remove('selecionado');
+    }
+
+    // 3 - novo item clicado deve ser marcado como selecionado 
+    item.classList.add('selecionado');
+    
+    //pegar titulo e preco do item
+    if ( seletor === '.pratos'){
+        tPrato = item.querySelector('.titulo').innerHTML;    
+        pPrato = item.querySelector('.preco').innerHTML;
+    }else if ( seletor === '.bebidas'){
+        tBebida = item.querySelector('.titulo').innerHTML;    
+        pBebida = item.querySelector('.preco').innerHTML;
+    }else{
+        tSobremesa = item.querySelector('.titulo').innerHTML;    
+        pSobremesa = item.querySelector('.preco').innerHTML;
+    }
+
+    liberarBotaoFinalizaPedido();
+}
+
+/*function selecionarPrato(prato){
     
     // 1 - verificar se existe um item selecionado anteriormente
     const itemJaSelecionado = document.querySelector('.pratos .selecionado');
@@ -39,6 +69,7 @@ function selecionarPrato(prato){
     
     //pegar titulo do prato
     tPrato = prato.querySelector('.titulo').innerHTML;    
+    pPrato = prato.querySelector('.preco').innerHTML;
 
     liberarBotaoFinalizaPedido();
 }
@@ -56,6 +87,7 @@ function selecionarBebida(bebida){
     bebida.classList.add('selecionado');
 
     tBebida = bebida.querySelector('.titulo').innerHTML;
+    pBebida = bebida.querySelector('.preco').innerHTML;
 
     liberarBotaoFinalizaPedido();
 }
@@ -73,6 +105,38 @@ function selecionarSobremesa(sobremesa){
     sobremesa.classList.add('selecionado');
 
     tSobremesa = sobremesa.querySelector('.titulo').innerHTML;
+    pSobremesa = sobremesa.querySelector('.preco').innerHTML;    
 
     liberarBotaoFinalizaPedido();
+}*/
+
+function fazerPedido(){
+
+    
+    let precoPrato = pPrato.replace('R$', '');
+    precoPrato = precoPrato.replace(',', '.');
+    precoPrato = Number(precoPrato);
+    
+    let precoBedida = pBebida.replace('R$', '');
+    precoBedida = precoBedida.replace(',', '.');
+    precoBedida = Number(precoBedida);
+
+    let precoSobremesa = pSobremesa.replace('R$', '');
+    precoSobremesa = precoSobremesa.replace(',', '.');
+    precoSobremesa = Number(precoSobremesa);
+
+    const total = precoPrato + precoBedida + precoSobremesa;
+
+    const  mensagem = `Olá, gostaria de fazer o pedido:
+    - Prato: ${tPrato}
+    - Bebida: ${tBebida}
+    - Sobremesa: ${tSobremesa}
+    Total: R$ ${total.toFixed(2)}`;    
+
+    // codificando a mesangem para o whatsapp
+    const mensWP = encodeURIComponent(mensagem);
+
+    // abrindo o WhatsApp e enviando a mesangem
+    window.open(`https://wa.me/5599999999999?text=${mensWP}`);
+
 }
